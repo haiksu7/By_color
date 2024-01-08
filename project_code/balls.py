0af52725fae6ea90df7447262fd10709e2a49613
haiksu7 <151848115+haiksu7@users.noreply.github.com>
2024-01-09 00:10:55 +0300

    update

--- project_code/balls.py
+++ project_code/balls.py
@@ -2,8 +2,9 @@ import pygame
 import sys
 import os
 
+
 def load_image(name, colorkey=None):
-    fullname = os.path.join('../data', name)
+    fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -11,7 +12,7 @@ def load_image(name, colorkey=None):
     if colorkey is not None:
         image = image.convert()
         if colorkey == -1:
-              colorkey = image.get_at((0, 0))
+            colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
     else:
         image = image.convert_alpha()
@@ -19,14 +20,16 @@ def load_image(name, colorkey=None):
 
 
 class Balls(pygame.sprite.Sprite):
-    def __init__(self, x, speed, filename):
+    def __init__(self, x, speed, surf, ind, group):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load_image(filename, (0, 0, 0))
+        self.image = surf
         self.rect = self.image.get_rect(center=(x, 0))
         self.speed = speed
+        self.ind = ind
+        self.add(group)
 
     def update(self, *args):
         if self.rect.y < args[0] - 20:
             self.rect.y += self.speed
         else:
-            self.rect.y = 0
+            self.kill()
